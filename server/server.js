0e537974e4b726eca444269a090edd0cf608d5a1
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-28 19:32:38 -0500

    more initializing with minor mods

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Setup body parser - to translating request body into JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve "static assets" (html, css, client-side js)
+// from the server/public folder
+app.use(express.static('server/public'));
+
+let tasksRouter = require('./routes/TDL.router');
+app.use('/TDL', tasksRouter);
+
+
+// Start express
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log('up and running on port', PORT);
+});
